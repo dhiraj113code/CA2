0a06c9652d963bd07f340627c6d99564408f31a4
dhiraj <dhiraj@doobie.(none)>
2013-10-04 00:07:21 -0400

    Added Fetch Stage

--- pipeline.c
+++ pipeline.c
@@ -43,4 +43,21 @@ dispatch(state_t *state) {
 
 void
 fetch(state_t *state) {
+//Input state variables - pc, fetch_lock
+//Output state variables - if_id
+
+unsigned long pc = state->pc;
+int i, instr;
+int machine_type, big_endian = 1, little_endian = 0;
+machine_type = little_endian;
+if(machine_type == big_endian)
+{
+   instr = (state->mem[pc] << 24) | (state->mem[pc+1] << 16) | (state->mem[pc+2] << 8) | (state->mem[pc+3]);
+}
+else if(machine_type == little_endian)
+{
+   instr = (state->mem[pc]) | (state->mem[pc+1] << 8) | (state->mem[pc+2] << 16) | (state->mem[pc+3] << 24);
+}
+state->if_id.instr = instr;
+state->if_id.pc = pc;
 }
